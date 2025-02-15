cb30d0959d9df4a3431b47fc53f1533c12daa400
imad-mahm <imadmahm333@gmail.com>
2025-02-15 13:04:44 +0200

    added all fies

---
+++ movies.js
@@ -0,0 +1,20 @@
+movies = {
+  1: {
+    title: "The Shawshank Redemption",
+    year: 1994,
+    director: "Frank Darabont",
+    rating: 9.3,
+  },
+  2: {
+    title: "The Godfather",
+    year: 1972,
+    director: "Francis Ford Coppola",
+    rating: 9.2,
+  },
+  3: {
+    title: "The Dark Knight",
+    year: 2008,
+    director: "Christopher Nolan",
+    rating: 9.0,
+  },
+};
